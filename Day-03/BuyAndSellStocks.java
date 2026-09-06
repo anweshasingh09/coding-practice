6d16fb1de3f80ec8b9f5c6c69efd2bf93f018e73
anweshasingh09 <anweshasingh0905@gmail.com>
2026-09-06 20:16:40 +0530

    Day 3: Buy and Sell Stocks

---
+++ Day-03/BuyAndSellStocks.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class BuyAndSellStocks {
+    public static int maxProfit(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String args[]){
+        int prices[]={7,1,5,8,6,3};
+        System.out.println(maxProfit(prices));
+    }
+}
